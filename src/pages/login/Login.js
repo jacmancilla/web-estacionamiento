188b0984360a36f2550e9d31c00ef0f76cbb1604
Katherine301 <ka.carrenoa@duocuc.cl>
2022-06-29 22:15:56 -0400

    Se realiza codigo de iniciar sesion (terminado).

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -6,8 +6,37 @@ import './style.css'
 const Login = () => {
     return (
         <div className="contenedor-inicio">
-            Iniciar sesión
+           <h2 class="parrafo1">Iniciar sesión</h2> 
+
+           <div class="Card">
+            <div class="texto1">
+                <label class="usu">Usuario:</label>
+                <input class="input1"></input>
+            </div>
+
+            <div class="texto2">
+                <label class="cont">Contraseña:</label>
+                <input ></input>
+            </div>
+
+            <div class="button0">
+                <button class="boton1">Iniciar Sesión</button>
+            </div>
+
+            <div class="olvcont">
+                <a class="etc" href="">¿Olvidaste contraseña?</a>
+            </div>
+            
+
+           </div>
+
+       
         </div>
+
+      
+
+        
+
     )
 }
 
