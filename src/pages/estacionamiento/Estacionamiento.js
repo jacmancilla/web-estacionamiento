7fb820f246705722e82cece2a2079c6e34de75d7
Katherine301 <ka.carrenoa@duocuc.cl>
2022-07-02 16:25:28 -0400

    se crea contenido de estacionamiento

---
+++ src/pages/estacionamiento/Estacionamiento.js
@@ -0,0 +1,11 @@
+import { Button } from '@mui/material'
+import React from 'react'
+import './style.css'
+
+const Estacionamiento = () =>{
+    return(
+        <div>desde Estacionamiento</div>
+    )
+}
+
+export default Estacionamiento
