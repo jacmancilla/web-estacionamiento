d8160f3e6f9ba2c2f634a1d333b1d3991c5bd40b
Jacqueline mancilla <80727358+jacmancilla@users.noreply.github.com>
2022-07-03 16:42:16 -0400

    se agrega login con usuarios  y ver comentarios

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,44 +1,85 @@
 import React from 'react'
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 import './style.css'
-
-
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+    const navigate = useNavigate()
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        const email = data.get('email')
+        const clave = data.get('password')
+        if (email === 'dueno@gmail.com' && clave === '112233') {
+            navigate('/micuentadueno')
+        }
+        if (email === 'cliente@gmail.com' && clave === '112233') {
+            navigate('/iniciocliente')
+        }
+    };
     return (
-        <div className="contenedor-inicio">
-           <h2 class="txt">Iniciar sesión</h2> 
+        <Container component="main" maxWidth="xs">
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Ingresar
+                </Typography>
+                <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Ingresa tu Email"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                        variant="filled"
+                    />
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Ingresa tu Clave"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                        variant="filled"
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                    >
+                        Iniciar Sesión
+                    </Button>
 
-           <div class="Card1">
-            <div class="texto1">
-                <label class="usua">Usuario:</label>
-                <input class="txt2"></input>
-            </div>
+                    <Link href="/recuperar" variant="body2">
+                        Olvidaste Tu Contraseña
+                    </Link>
 
-            <div class="txt3">
-                <label class="contras">Contraseña:</label>
-                <input ></input>
-            </div>
-
-            <div class="but0">
-                <button class="btn">Iniciar Sesión</button>
-            </div>
-
-            <div class="olvcontra">
-                <a class="ettc" href="/recuperar">¿Olvidaste contraseña?</a>
-            </div>
-            
-
-           </div>
-
-       
-        </div>
-
-      
-
-        
-
+                </Box>
+            </Box>
+        </Container>
     )
 }
-
 
 export default Login
---
+++ src/pages/vercomentarios/Vercomentarios.js
@@ -0,0 +1,64 @@
+import { Button } from '@mui/material'
+import React from 'react'
+import './style.css'
+
+const Vercomentarios = () => {
+
+    return (
+        <div>
+
+            <div className="titulocomentarioscli">
+                <h2>Comentarios de clientes:</h2>
+            </div>
+
+            <div className="cards-list">
+
+                <div className="cards 1">
+                    <div class="card_title title-black">
+                        <p>Manuel Soto</p>
+                    </div>
+
+                    <div class="card_parrafo title-black">
+                        <p>Estacionamiento espacioso, buen trato con el dueño.</p>
+                    </div>
+                </div>
+
+                <div class="cards 2">
+                    <div class="card_title title-black">
+                        <p>Carla Díaz</p>
+                    </div>
+
+                    <div class="card_parrafo title-black">
+                        <p>Estacionamiento con poca visibilidad, demarcado y espacioso.</p>
+                    </div>
+                </div>
+
+                <div class="cards 3">
+                    <div class="card_title">
+                        <p>Claudia Martinez</p>
+                    </div>
+
+                    <div class="card_parrafo title-black">
+                        <p>Estacionamiento con poca demarcaciones, dificil acceso a la zona del estacionamiento.</p>
+                    </div>
+                </div>
+
+                <div class="cards 4">
+                    <div class="card_title title-black">
+                        <p>Francisco Muñoz</p>
+                    </div>
+
+                    <div class="card_parrafo title-black">
+                        <p>Estacionamiento con buena visibilidad, espacioso, demarcaciones en buen estado y limpio. </p>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    )
+}
+
+
+export default Vercomentarios
