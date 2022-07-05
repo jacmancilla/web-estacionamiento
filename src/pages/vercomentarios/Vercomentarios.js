bf131025f5a5e0b44589ed848377df39213c6c65
Katherine301 <ka.carrenoa@duocuc.cl>
2022-07-04 21:07:55 -0400

    Se modifica css de ver comentarios dueño

--- src/pages/vercomentarios/Vercomentarios.js
+++ src/pages/vercomentarios/Vercomentarios.js
@@ -13,42 +13,42 @@ const Vercomentarios = () => {
 
             <div className="cards-list">
 
-                <div className="cards 1">
+                <div className="cardsf 1">
                     <div class="card_title title-black">
                         <p>Manuel Soto</p>
                     </div>
 
-                    <div class="card_parrafo title-black">
+                    <div class="card_parrafot title-black">
                         <p>Estacionamiento espacioso, buen trato con el dueño.</p>
                     </div>
                 </div>
 
-                <div class="cards 2">
+                <div class="cardsf 2">
                     <div class="card_title title-black">
                         <p>Carla Díaz</p>
                     </div>
 
-                    <div class="card_parrafo title-black">
+                    <div class="card_parrafot title-black">
                         <p>Estacionamiento con poca visibilidad, demarcado y espacioso.</p>
                     </div>
                 </div>
 
-                <div class="cards 3">
+                <div class="cardsf 3">
                     <div class="card_title">
                         <p>Claudia Martinez</p>
                     </div>
 
-                    <div class="card_parrafo title-black">
+                    <div class="card_parrafot title-black">
                         <p>Estacionamiento con poca demarcaciones, dificil acceso a la zona del estacionamiento.</p>
                     </div>
                 </div>
 
-                <div class="cards 4">
+                <div class="cardsf 4">
                     <div class="card_title title-black">
                         <p>Francisco Muñoz</p>
                     </div>
 
-                    <div class="card_parrafo title-black">
+                    <div class="card_parrafot title-black">
                         <p>Estacionamiento con buena visibilidad, espacioso, demarcaciones en buen estado y limpio. </p>
                     </div>
                 </div>
